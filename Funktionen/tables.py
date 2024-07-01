1b5aae9717f4d5e0d0932cbcabf1cc97e4195456
ekrepper <e.krepper@mci4me.at>
2024-07-01 12:41:41 +0200

    fixes db

---
+++ Funktionen/tables.py
@@ -0,0 +1,77 @@
+import sqlite3
+import fitparse as ff  # oder der passende Fitfile Parser
+import pandas as pd
+
+def create_table():
+    conn = sqlite3.connect('fitfile_data.db')
+    c = conn.cursor()
+    create_table_sql = """
+        CREATE TABLE IF NOT EXISTS trainings (
+            activity_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            activity_date DATE,
+            activity_kw INTEGER,
+            activity_duration TIME,
+            activity_total_distance FLOAT,
+            activity_avg_pace TIME,
+            activity_avg_hr INTEGER
+        );
+    """
+    c.execute(create_table_sql)
+    conn.commit()
+    conn.close()
+
+def insert_data(uploaded_file):
+    conn = sqlite3.connect('fitfile_data.db')
+    c = conn.cursor()
+    fit_parser = ff.FitFile(uploaded_file)
+    
+    for record in fit_parser.get_messages('record'):
+        activity_date = record.get_value('timestamp')
+        activity_duration = record.get_value('timer_time')
+        activity_total_distance = record.get_value('distance')
+        activity_avg_pace = record.get_value('enhanced_avg_speed')
+        activity_avg_hr = record.get_value('avg_heart_rate')
+        
+        # Einfügeabfrage mit Überprüfung auf Duplikate
+        insert_sql = """
+            INSERT OR IGNORE INTO trainings (
+                activity_date, activity_kw, activity_duration, activity_total_distance, activity_avg_pace, activity_avg_hr
+            ) VALUES (?, ?, ?, ?, ?, ?);
+        """
+        try:
+            c.execute(insert_sql, (activity_date, activity_date.isocalendar()[1], activity_duration, activity_total_distance, activity_avg_pace, activity_avg_hr))
+        except sqlite3.Error as e:
+            print(f"Fehler beim Einfügen der Daten in die Datenbank: {e}")
+    
+    conn.commit()
+    conn.close()
+
+def get_training_data():
+    conn = sqlite3.connect('fitfile_data.db')
+    query = """
+        SELECT activity_kw, activity_date, SUM(activity_total_distance) AS total_distance
+        FROM trainings
+        GROUP BY activity_kw, activity_date
+        ORDER BY activity_date
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+
+def delete_entry(activity_id):
+    conn = sqlite3.connect('fitfile_data.db')
+    c = conn.cursor()
+    
+    delete_sql = """
+        DELETE FROM trainings
+        WHERE activity_id = ?
+    """
+    try:
+        c.execute(delete_sql, (activity_id,))
+        conn.commit()
+        print("Eintrag erfolgreich gelöscht.")
+    except sqlite3.Error as e:
+        print(f"Fehler beim Löschen des Eintrags: {e}")
+    finally:
+        conn.close()
