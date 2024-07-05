c8ca4dc07983adb5a18e2b7bc299f3a703a75215
Ihr Name auf github <Ihre E-Mail von github>
2024-07-05 22:04:22 +0200

    Fit_files neu Struktoriert

--- Funktionen/fit_files.py
+++ Funktionen/fit_files.py
@@ -2,92 +2,138 @@ import fitparse
 import numpy as np
 import datetime as datetime
 import streamlit as st
+import pandas as pd
 
 class FitFile:
-    def __init__(self, fit_file):
-        self.fit_file = fit_file
+    def __init__(self, fit_file, user_id = None):
+        self.fit_file = fitparse.FitFile(fit_file)
         self.dateref = datetime.datetime(1970, 1, 1)
-        self.heartrate = np.array([])
-        self.time = np.array([])
-        self.distance = np.array([])
-        self.avg_hr = None
-        self.avg_speed = None
-        self.total_distance = None
-        self.total_timer_time = None
-        self.timestamp = None
-        self.load_fit_file()
+        self.heartrate = self.get_heartrate()
+        self.time = self.get_time()
+        self.distance = self.get_distance()
+        self.avg_hr = self.get_avg_hr()
+        self.avg_speed = self.get_avg_speed()    
+        self.total_distance = self.get_total_distance()
+        self.total_timer_time = self.get_total_time()
+        self.timestamp = self.get_timestamp()
+        self.date = self.get_date()
+        self.calendar_week = self.get_calendar_week()
+        self.heartrate_zones = self.get_heartrate_zones(211)  # 220 - 30 = 190 (max. Herzfrequenz)
+        self.time_zone_1 = self.get_time_in_zones(self.heartrate_zones["Zone 1"])
+        self.time_zone_2 = self.get_time_in_zones(self.heartrate_zones["Zone 2"])
+        self.time_zone_3 = self.get_time_in_zones(self.heartrate_zones["Zone 3"])
+        self.time_zone_4 = self.get_time_in_zones(self.heartrate_zones["Zone 4"])
+        self.time_zone_5 = self.get_time_in_zones(self.heartrate_zones["Zone 5"])
+        self.user_id = user_id
 
-    def load_fit_file(self):
-        try:
-            fitfile = fitparse.FitFile(self.fit_file)
-        except Exception as e:
-            st.error(f"Error loading FIT file: {e}")
-            return
-        
-        for record in fitfile.get_messages("record"):
+    def get_heartrate(self):
+        heartrate = np.array([])
+        for record in self.fit_file.get_messages("record"):
             for data in record:
                 if data.name == 'heart_rate':
-                    self.heartrate = np.append(self.heartrate, data.value)
-                elif data.name == 'timestamp':
-                    self.time = np.append(self.time, (data.value - self.dateref).total_seconds())
-                elif data.name == 'distance':
-                    self.distance = np.append(self.distance, data.value)
-        
-        for session in fitfile.get_messages("session"):
-            for data in session:
-                if data.name == "avg_heart_rate":
-                    self.avg_hr = data.value
-                if data.name == "avg_speed":
-                    self.avg_speed = data.value
-                if data.name == "total_distance":
-                    self.total_distance = data.value
-                if data.name == "total_timer_time":
-                    self.total_timer_time = data.value
-                if data.name == "timestamp":
-                    self.timestamp = data.value
-
-    def get_total_distance(self):
-        if self.total_distance is not None:
-            total_distance_km = self.total_distance / 1000
-            return total_distance_km
-        else:
-            return None
+                    heartrate = np.append(heartrate, data.value)
+                else:
+                     continue
+        return heartrate
+    
+    def get_time(self):
+        time = np.array([])
+        for record in self.fit_file.get_messages("record"):
+            for data in record:
+                if data.name == 'timestamp':
+                    time = np.append(time, (data.value - self.dateref).total_seconds())
+                else:
+                     continue
+        return time
+    
+    def get_distance(self):
+        distance = np.array([])
+        for record in self.fit_file.get_messages("record"):
+            for data in record:
+                if data.name == 'distance':
+                    distance = np.append(distance, data.value)
+                else:
+                     continue
+        return distance
     
     def get_avg_hr(self):
-        return self.avg_hr
+        for session in self.fit_file.get_messages("session"):
+            for data in session:
+                if data.name == "avg_heart_rate":
+                    avg_hr = data.value
+                else:
+                    continue
+        return avg_hr
+
+    def get_total_distance(self):
+        for session in self.fit_file.get_messages("session"):
+            for data in session:
+                if data.name == "total_distance":
+                    total_distance = data.value
+                else:
+                    continue
+        return total_distance / 1000  # Umrechnen von m in km   
     
-    def get_avg_pace(self):
-        if self.avg_speed is not None:
-            pace = 60 / (self.avg_speed * 3.6)  # Umrechnen von m/s in km/h und dann in min/km
-            minutes = int(pace)
-            seconds = int((pace - minutes) * 60)
-            return f"{minutes}:{seconds:02d}"
-        else:
-            return None
+    def get_avg_speed(self):
+        for session in self.fit_file.get_messages("session"):
+            for data in session:
+                if data.name == "avg_speed":
+                    avg_speed = data.value
+                else:
+                    continue
+        pace = 60 / (avg_speed * 3.6)  # Umrechnen von m/s in km/h und dann in min/km
+        minutes = int(pace)
+        seconds = int((pace - minutes) * 60)
+        return f"{minutes}:{seconds:02d}"  
         
     def get_total_time(self):
-        if self.total_timer_time is not None:
-            hours = int(self.total_timer_time // 3600)
-            minutes = int((self.total_timer_time % 3600) // 60)
-            seconds = int(self.total_timer_time % 60)
-            total_time = f"{hours:02}:{minutes:02}:{seconds:02}"
-            return total_time
-        else:
-            return None
+        for session in self.fit_file.get_messages("session"):
+            for data in session:
+                if data.name == "total_timer_time":
+                    total_timer_time = data.value
+                else:
+                    continue
+        hours = int(total_timer_time // 3600)
+        minutes = int((total_timer_time % 3600) // 60)
+        seconds = int(total_timer_time % 60)
+        total_time = f"{hours:02}:{minutes:02}:{seconds:02}"
+        return total_time
+    
+    def get_timestamp(self):
+        for session in self.fit_file.get_messages("session"):
+            for data in session:
+                if data.name == "timestamp":
+                    timestamp = data.value
+                else:
+                    continue
+        return timestamp
     
     def get_date(self):
-        if self.timestamp is not None:
-            date_str = self.timestamp.strftime('%Y-%m-%d')
-            return date_str
-        else:
-            return None
-    
+        date_str = self.timestamp.strftime('%Y-%m-%d')
+        return date_str
+  
     def get_calendar_week(self):
-        if self.timestamp is not None:
-            calendar_week = self.timestamp.isocalendar()[1]
-            return calendar_week
-        else:
-            return None
+        calendar_week = self.timestamp.isocalendar()[1]
+        return calendar_week
+
+    def get_heartrate_zones(self, max_heartrate):
+        zone_1 = np.where(self.heartrate < 0.6 * max_heartrate)
+        zone_2 = np.where(np.logical_and(self.heartrate >= 0.6 * max_heartrate, self.heartrate < 0.7 * max_heartrate))
+        zone_3 = np.where(np.logical_and(self.heartrate >= 0.7 * max_heartrate, self.heartrate < 0.8 * max_heartrate))  
+        zone_4 = np.where(np.logical_and(self.heartrate >= 0.8 * max_heartrate, self.heartrate < 0.9 * max_heartrate))
+        zone_5 = np.where(self.heartrate >= 0.9 * max_heartrate)
+        data = {"Zone 1" : zone_1[0],
+                "Zone 2" : zone_2[0],
+                "Zone 3" : zone_3[0],
+                "Zone 4" : zone_4[0],
+                "Zone 5" : zone_5[0]}
+        return data
+    
+    def get_time_in_zones(self, data):
+        time_zone = np.size(data)
+        minutes = int((time_zone % 3600) // 60)
+        seconds = int(time_zone % 60)
+        return f"{minutes:02}:{seconds:02}"
 
     def get_insert_statement(self):
         """Erstellt eine SQL-Insert-Anweisung für die trainings-Tabelle basierend auf den gesammelten Daten"""
@@ -102,14 +148,23 @@ class FitFile:
                 activity_duration,
                 activity_total_distance,
                 activity_avg_pace,
-                activity_avg_hr
+                activity_avg_hr,
+                user_id
             ) VALUES (
-                '{self.get_date()}',
-                {self.get_calendar_week()},
-                '{self.get_total_time()}',
-                {self.get_total_distance()},
-                '{self.get_avg_pace()}',
-                {self.get_avg_hr()}
+                '{self.date}',
+                {self.calendar_week},
+                '{self.total_timer_time}',
+                {self.total_distance},
+                '{self.avg_speed}',
+                {self.avg_hr},
+                {self.user_id}
             );
         """
         return insert_sql
+
+
+if __name__ == "__main__":
+    fit = FitFile("data/Fit_files/Running_2024-06-04T13_16_40-1.fit")
+    print(fit.avg_hr)
+    print(fit.avg_speed)   
+    print(fit.total_distance)   
